c2d4e5612b63347134ae44ed1b28de14aa26f3e6
Marlene Guzman <marleneguzmanla@gmail.com>
2019-01-04 18:48:02 -0800

    for youtube api save. adding again

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = "AIzaSyBcDxjZsQBP256Jlobgkp2yQ69RweMyfv4";
+
+export default axios.create({
+baseURL:  'https://www.googleapis.com/youtube/v3',
+params: {
+  part: 'snippet',
+  maxResults: 5,
+  key: KEY,
+}
+});
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
- 
-  return <div>{props.videos.length}</div>
+const VideoList = ({ videos }) => {
+  const renderedLIst = videos.map(video => {
+    return <VideoItem video={video}/>
+  });
+  return <div>{renderedLIst}</div>
 };
 
 export default VideoList;
