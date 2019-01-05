e12c7b247cf0b4176a0372cc616d1ee1529c800e
Marlene Guzman <marleneguzmanla@gmail.com>
2019-01-04 23:22:27 -0800

    display video playes and add more css

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -10,17 +10,23 @@ state = {
   selectedVideo: null 
 };
 
+componentDidMount(){
+  this.onTermSubmit('children stories');
+}
+
 onTermSubmit = async term => {
  const response = await youtube.get('/search', {
     params: {
       q: term
     }
   });
-  this.setState({ videos: response.data.items })
+ this.setState({
+   videos: response.data.items,
+   selectedVideo: response.data.items[0]
+ });
 };
 
 onVideoSelect = video => {
-// console.log('from the app', video)
 this.setState({ selectedVideo: video })
 
 }
@@ -29,8 +35,16 @@ this.setState({ selectedVideo: video })
     return (
     <div className='ui container'>
       <SearchBar onFormSubmit={this.onTermSubmit}/>
-      <VideoDetail video={this.state.selectedVideo}/>
-    <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={this.state.selectedVideo}/>
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+          </div>
+          </div>
+        </div>
     </div>
     )
   }
